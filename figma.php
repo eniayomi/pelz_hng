70a832f5c14af752d62bb2aece7f82194dc91018
Oluwaseyi Oluwapelumi <nathanoluwaseyi@gmail.com>
2018-04-14 19:40:30 +0100

    Initial commit

---
+++ figma.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+body {
+    background-image: url("pelzy.jpg");
+	
+}
+.main {
+    color: #14DD6A;
+}
+.main {
+    font-size: 40px;
+}
+.main {
+font-style: Roboto;
+}
+.main {
+   position: absolute;
+    left: 0;
+    top: 50%;
+    width: 100%;
+    text-align: center;
+	}
+.date {
+color: #EC0D0D;
+}	
+.date {
+font-size: 25px;
+}
+.date {
+position: absolute;
+    left: 0;
+    top: 65%;
+    width: 100%;
+    text-align: center;
+}
+.date {
+font-style: Roboto;
+}
+
+.foot {
+    color: #60FFA4;
+}
+.foot {
+    font-size: 40px;
+}
+.foot {
+font-style: Roboto;
+}
+.foot {
+   position: absolute;
+    left: 0;
+    top: 85%;
+    width: 100%;
+    text-align: center;
+	}
+.time {
+color: #EC0D0D;
+}	
+.time {
+font-size: 40px;
+}
+.time {
+position: absolute;
+    left: 0;
+    top:70%;
+    width: 100%;
+    text-align: center;
+}
+.time {
+font-style: Roboto;
+}	
+</style>
+<div class="main">
+<p><b><center> HNGINTERNSHIPS 4.0 </center><b></p>
+</div>
+
+<div class="date">
+
+<p id="date_time"></p>
+<script>
+document.getElementById("date_time").innerHTML = Date();
+</script>
+</div>
+
+<div class="foot">
+<p> Unleashing Innovations </p>
+</div>
+
+<div class="time">
+</div>
+</body>
+</html>
